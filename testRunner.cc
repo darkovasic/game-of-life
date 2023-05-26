d360a4b8f3b6d21e036167a7376d483453d88818
Darko Vasic <darko.vasic@htecgroup.com>
2023-05-26 13:56:00 +0200

    SetInitialState test added

--- testRunner.cc
+++ testRunner.cc
@@ -12,6 +12,27 @@ TEST(GridTest, Initialization) {
   }
 }
 
+TEST(GridTest, SetInitialState) {
+  Grid grid(5, 5);
+
+  // Set specific cells as alive
+  grid.setCell(2, 2, true);
+  grid.setCell(3, 3, true);
+
+  // Check the state of the cells
+  EXPECT_EQ(grid.getCell(2, 2).isAlive(), true);
+  EXPECT_EQ(grid.getCell(3, 3).isAlive(), true);
+
+  // Check that other cells are still dead
+  for (int row = 0; row < 5; ++row) {
+    for (int col = 0; col < 5; ++col) {
+      if (row != 2 && col != 2 && row != 3 && col != 3) {
+        EXPECT_EQ(grid.getCell(row, col).isAlive(), false);
+      }
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
